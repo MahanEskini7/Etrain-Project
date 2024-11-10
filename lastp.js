f5ed0fb01fbd96aa7aef312c17b6916f7d37a76f
Mahan Eskini <mahan.es1387.mahan@gmail.com>
2024-11-10 22:20:12 +0330

    init Etrain project

---
+++ lastp.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".hamburgerMenu")
+var navbar = document.querySelector(".navbarMenu")
+menuIcon.addEventListener("click" , ()=>{
+    navbar.classList.toggle("change")
+})
+
